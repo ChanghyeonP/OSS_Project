8edce04f40d418ebfd8c9724f7493fa9f2f5d7e5
Codingdo <2118681@naver.com>
2024-05-22 21:00:40 +0900

    Change Chatroom name

--- noticeBot.py
+++ noticeBot.py
@@ -16,7 +16,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from logging.handlers import TimedRotatingFileHandler
 
 # # 카톡창 이름, (활성화 상태의 열려있는 창)
-kakao_opentalk_name = 'noticebot'
+kakao_opentalk_name = 'test'
 idx = 0
 
 
