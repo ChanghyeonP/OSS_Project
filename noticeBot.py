d650085dbe9c921da91030b806df3e1190a08b52
ChangyhyeonP <parkchang12332@gmail.com>
2024-05-22 18:11:29 +0900

    Changing the Crawling Algorithm

--- noticeBot.py
+++ noticeBot.py
@@ -7,7 +7,11 @@ import requests
 import json
 import logging
 
-from operator import eq
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
 from apscheduler.schedulers.background import BackgroundScheduler
 from logging.handlers import TimedRotatingFileHandler
 
@@ -66,57 +70,44 @@ def open_chatroom(chatroom_name):
 # 공지사항 크롤링하기
 
 
-def get_dwu_notice():
+def get_all_notices():
+    # Chrome 드라이버 설정
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-    today = datetime.datetime.today().strftime("%Y%m%d")
-    url = 'https://www.dongduk.ac.kr/ajax/board/kor/kor_notice/list.json'
-    req = requests.get(url)
+    today = datetime.date.today().strftime("%Y-%m-%d")
 
-    html = req.text
+    # 원하는 URL 열기
+    driver.get('https://www.daegu.ac.kr/article/DG159/list?pageIndex=1&')
 
-    data = json.loads(html)
+    # 모든 공지사항 요소 가져오기
+    notice_elements = driver.find_elements(By.CSS_SELECTOR, '#sub_contents > div > table > tbody > tr')
+    
+    # 공지사항을 담을 리스트 초기화
+    all_notices = []
 
-    temp = data.get('data')
-    rslt = temp.get('list')
-
-    # 만약 제목이 같으면 최신 공지만 전송하도록 하기 위해 global_title 변수 사용
-    global global_title
-    global_title = '공지방 홧팅'
-
-    noticeList = []
-    for i in rslt:
-        date = i.get('REG_DT')[:8]
-        index = i.get('B_IDX')
-        title = i.get('B_TITLE')
-        href = 'https://www.dongduk.ac.kr/board/kor/kor_notice/detail.do?curPageNo=1&pageStatus=N&rowSize=15&B_IDX=' + \
-            str(index)
-
-        # 최신 공지만 noticeList에 추가하도록 판별
-        if eq(title, global_title):
+    # 각 공지사항 요소에서 제목과 링크 추출하여 리스트에 추가
+    for idx, element in enumerate(notice_elements, start=1):
+        if idx <= 12:  # 12번째 공지사항은 리스트에 추가하지 않음
             continue
-        else:
-            global_title = title
+        title_element = element.find_element(By.CSS_SELECTOR, 'td.list_left > a')
+        date_element = element.find_element(By.CSS_SELECTOR, 'td:nth-child(5)')  # 공지사항의 날짜 요소 선택
+        title = title_element.text.strip()
+        date = date_element.text.strip()
+        link = title_element.get_attribute('href')
 
-        # 공지사항 리스트
-        if eq(today, date):
-            rslt = "[" + date + "]\n" + title + "\n" + href
-            noticeList.append(rslt)
+        if date == today:
+            all_notices.append({'date': date, 'title': title, 'link': link})    # 브라우저 닫기
+    
+    driver.quit()
 
-    # 리스트 역순 정렬
-    noticeList.reverse()
-
-    return noticeList
+    return all_notices
 
 
 # # 스케줄러 job : 매 시간마다 공지사항 크롤링해서 가져오기
 def job():
-    p_time_ymd_hms = \
-        f"{time.localtime().tm_year}-{time.localtime().tm_mon}-{time.localtime().tm_mday} / " \
-        f"{time.localtime().tm_hour}:{time.localtime().tm_min}:{time.localtime().tm_sec}"
-
     # 채팅방 열기
     open_chatroom(kakao_opentalk_name)
-    noticeList = get_dwu_notice()
+    noticeList = get_all_notices()
 
     # 메시지 전송, time/실검
     kakao_sendtext(kakao_opentalk_name, noticeList)
